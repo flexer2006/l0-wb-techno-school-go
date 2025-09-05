d94e11caba2bd585d0c7159243b4e1c9b2080f24
Andrew <andrewgo1133official@gmail.com>
2025-09-05 15:00:22 +0000

    Refactor

--- internal/adapters/server/handlers/order.go
+++ internal/adapters/server/handlers/order.go
@@ -1,8 +1,6 @@
 package handlers
 
 import (
-	"context"
-
 	"github.com/flexer2006/l0-wb-techno-school-go/internal/logger"
 	"github.com/gofiber/fiber/v3"
 
@@ -13,23 +11,23 @@ func OrderHandler(cache ports.Cache, repo ports.OrderRepository, log logger.Logg
 	return func(ctx fiber.Ctx) error {
 		orderUID := ctx.Params("id")
 		if orderUID == "" {
-			log.Warn("empty order UID in request")
+			log.WithContext(ctx).Warn("empty order UID in request")
 			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Order UID is required"})
 		}
 
 		if order, found := cache.Get(orderUID); found {
-			log.Debug("order found in cache", "order_uid", orderUID)
+			log.WithContext(ctx).Debug("order found in cache", "order_uid", orderUID)
 			return ctx.JSON(order)
 		}
 
-		order, err := repo.GetOrder(context.Background(), orderUID)
+		order, err := repo.GetOrder(ctx, orderUID)
 		if err != nil {
-			log.Error("failed to get order from DB", "order_uid", orderUID, "error", err)
+			log.WithContext(ctx).Error("failed to get order from DB", "order_uid", orderUID, "error", err)
 			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Order not found"})
 		}
 
 		cache.Set(order)
-		log.Info("order retrieved from DB and cached", "order_uid", orderUID)
+		log.WithContext(ctx).Info("order retrieved from DB and cached", "order_uid", orderUID)
 
 		return ctx.JSON(order)
 	}
